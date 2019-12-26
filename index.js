04eb9eabf6beb82df6f1e5ac5d2d2ff478185c99
Utkarsh Yadav <yadavutkarsh207@gmail.com>
2019-12-27 00:46:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+const express =require("express");
+const bodyParser=require("body-parser");
+const request = require("request");
+
+
+const app=express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function (req, res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+
+app.post("/", function(req, res){
+  var crypto = req.body.crypto;
+  var fiat = req.body.fiat;
+  var amount = req.body.amount;
+
+  var baseURL = "https://apiv2.bitcoinaverage.com/convert/global";
+  var options = {
+    url: "https://apiv2.bitcoinaverage.com/convert/global",
+    method: "GET",
+    qs: {
+      from: crypto,
+      to: fiat,
+      amount: amount
+    }
+  };
+
+  request(options, function(error, response, body){
+    var data = JSON.parse(body);
+    var price = data.price;
+    console.log(price);
+
+    var currentTime = data.time;
+
+    res.write("<p>the current date is " + currentTime + " </p>")
+
+    res.write("<h1>the current price of " + amount + " "+ crypto + " is currently worth " + price + fiat + "</h1>")
+
+    res.send();
+  });
+});
+
+
+
+app.listen(3000, function (){
+  console.log("server is running on port 3000");
+});
